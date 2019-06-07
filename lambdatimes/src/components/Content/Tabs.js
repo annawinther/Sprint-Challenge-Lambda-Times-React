960eb53c633dab2be7fa04203250911b0de84bdd
Anna W <anski.anna@gmail.com>
2019-06-07 09:07:03 +0100

    made the Tabs components and the Cards component with mapping over the cards prop

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
+import pt from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +9,27 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {props.tabs.map((tab, index) => (
+              <Tab 
+              key={index}
+              selectTabHandler={props.selectTabHandler}
+              selectedTab={props.selectedTab}
+              tab={tab}
+              />
+            )
+              
+            )}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.PropType = {
+  tabs: pt.arrayOf(pt.string),
+  tab: pt.string,
+  selectedTab: pt.string,
+
+}
+
 export default Tabs;
